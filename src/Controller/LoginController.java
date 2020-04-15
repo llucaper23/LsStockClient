dae09e37e55bc86ffde0130252857c7582e3f7e6
llucaper23 <llucaper@gmail.com>
2020-04-16 01:01:26 +0200

    mirar info registre

--- src/Controller/LoginController.java
+++ src/Controller/LoginController.java
@@ -8,6 +8,11 @@ import View.SignUpWindow;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class LoginController implements ActionListener {
     private Network network;
@@ -30,14 +35,14 @@ public class LoginController implements ActionListener {
                 break;
             case "REGISTER":
                 User aux = new User(signupView.getName(), signupView.getMail(), signupView.getPassword(), 0, false);
-                if (manager.comprovaUser(aux)) {
+                if (comprovaUser(aux)) {
                     User user = aux;
                     //ok = network.registraUsuari(user); PROC PER REGISTRAR USUARI AMB EL SOCKET
                     if (ok) {
                         signupView.dispose();
                         loginView.setVisible(true);
                     } else {
-                        signupView.mostraMissatgeError("Error with the info introduced");
+                        signupView.mostraMissatgeError("Error checking the info");
                     }
                 }
                 break;
@@ -47,6 +52,55 @@ public class LoginController implements ActionListener {
                 signupView.registrarControlador(this);
                 signupView.setVisible(true);
                 break;
+        }
+    }
+
+    public boolean comprovaUser(User user) {
+        boolean check = false;
+        Pattern pattern = Pattern.compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+
+        // El email a validar
+        String email = user.getEmail();
+
+        Matcher mather = pattern.matcher(email);
+
+        if (!mather.find()) {
+            signupView.mostraMissatgeError("Mail not valid");
+            check = false;
+        }
+
+        if (check){
+            if (signupView.getPassword().equals(signupView.getPasswordCheck())) {
+                check = manager.checkPassword(signupView.getPassword());
+            } else {
+                signupView.mostraMissatgeError("Passwords must be equals");
+                check = false;
+            }
+        }
+
+        if (check){
+            user.setPassword(getMD5(signupView.getPassword()));
+        }else{
+            signupView.mostraMissatgeError("Passwrod not accomplish the requisits");
+        }
+        return check;
+    }
+
+    public static String getMD5(String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] messageDigest = md.digest(input.getBytes());
+            BigInteger number = new BigInteger(1, messageDigest);
+            String hashtext = number.toString(16);
+
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+            return hashtext;
+        }
+        catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
         }
     }
 }
--- src/Model/Manager.java
+++ src/Model/Manager.java
@@ -4,24 +4,18 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Manager {
-
     private final String regex = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$";
 
-    public Manager() {
-    }
+    /**
+     * Funcio que comprova si el password compleix els requeriments
+     * @param passwrod es el password a comprovar
+     */
 
-    public boolean comprovaPassword(String passwrod){
+    public boolean checkPassword(String passwrod){
         Pattern p = Pattern.compile(regex, Pattern.MULTILINE);
         Matcher m = p.matcher(passwrod);
-        return m.matches();
-    }
-
-    public boolean comprovaUser(User user){
-        boolean ok = false;
-
-
+        boolean ok = m.matches();
         return ok;
     }
-
 
 }
