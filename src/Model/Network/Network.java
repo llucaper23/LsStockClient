c72361c480e66959c5253fc4c73a6f9265503345
llucaper23 <llucaper@gmail.com>
2020-04-29 17:19:53 +0200

    Canvis network fets per carol pero no li va el git

--- src/Model/Network/Network.java
+++ src/Model/Network/Network.java
@@ -1,21 +1,26 @@
 package Model.Network;
 
 
-import Model.User;
-import com.google.gson.Gson;
-import com.google.gson.stream.JsonReader;
+        import Controller.LoginController;
+        import Model.Company;
+        import Model.Manager;
+        import Model.User;
+        import com.google.gson.Gson;
+        import com.google.gson.stream.JsonReader;
 
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.Socket;
+        import java.io.FileReader;
+        import java.io.IOException;
+        import java.io.ObjectInputStream;
+        import java.io.ObjectOutputStream;
+        import java.net.Socket;
+        import java.util.ArrayList;
 
-public class Network {
+public class Network extends Thread {
 
     private Socket socket;
     private ObjectInputStream ois;
     private ObjectOutputStream oos;
+    private boolean isOn;
 
     private static final int REGISTER_REQUEST = 1;
     private static final int LOGIN_REQUEST = 2;
@@ -27,6 +32,7 @@ public class Network {
     private static final int COMPANY_DETAIL = 8;
     private static final int LOGOUT = 9;
 
+    private boolean running;
     private NetworkConfiguration nc;
 
     public Network() {
@@ -51,16 +57,63 @@ public class Network {
 
     }
 
+    public void startClientNetwork() {
+        // iniciem la comunicacio amb el servidor
+        isOn = true;
+        this.start();
+    }
+
+    public void stopClientNetwork() {
+        // aturem la comunicacio amb el servidor
+        this.isOn = false;
+        this.interrupt();
+    }
+
+    public void run() {
+        while (isOn) {
+            try {
+                int opt = ois.readInt();
+                switch (opt) {
+                    case ALL_COMPANIES:
+                        updateAllCompanies();
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                // si hi ha algun problema satura la comunicacio amb el servidor
+                stopClientNetwork();
+            }
+        }
+        stopClientNetwork();
+    }
+
+    private void updateAllCompanies() {
+
+        try {
+            ArrayList<Company> updatedCompanies = new ArrayList<>();
+            int size = ois.readInt();
+            for (int i = 0; i < size; i++) {
+                updatedCompanies.add((Company) ois.readObject());
+            }
+            //aqui ja tens una arraylist de companyies actualitzades
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
     public boolean registraUsuari(User user) {
         try {
             oos.write(REGISTER_REQUEST);
             oos.flush();
             oos.writeObject(user);
             boolean infoOk = ois.readBoolean();
-            if (!infoOk){
+            if (!infoOk) {
                 System.out.println("Error to register");
                 return false;
-            }else{
+            } else {
                 System.out.println("Register Succesful!");
                 return true;
             }
@@ -71,36 +124,27 @@ public class Network {
 
     }
 
-    public User loginUsuari(User user){
+    public User loginUsuari(User user) {
         try {
             oos.write(LOGIN_REQUEST);
             oos.flush();
             oos.writeObject(user);
             oos.flush();
             boolean loginOk = ois.readBoolean();
-            if (!loginOk){
+            if (!loginOk) {
                 System.out.println("Error to login");
                 return null;
-            }else{
+            } else {
                 return (User) ois.readObject();
             }
-        }catch (IOException | ClassNotFoundException e){
+        } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
         return null;
     }
 
-    public void logout(){
-        try {
-            oos.write(LOGOUT);
-            oos.flush();
-        }catch (IOException e){
-            e.printStackTrace();
-        }
-    }
 
     public void buyShares () {
-
         try {
             oos.write(BUY_SHARES);
             oos.flush();
@@ -111,6 +155,4 @@ public class Network {
         }
 
     }
-
-
 }
